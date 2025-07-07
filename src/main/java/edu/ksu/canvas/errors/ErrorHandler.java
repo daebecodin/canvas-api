72356482425ec79f35cef5bb725b3cb521ce41cf
daebecodin <136534615+daebecodin@users.noreply.github.com>
2025-07-07 01:02:55 -0700

    migrating ErrorHandler.java to use apache Http 5 Classic

--- src/main/java/edu/ksu/canvas/errors/ErrorHandler.java
+++ src/main/java/edu/ksu/canvas/errors/ErrorHandler.java
@@ -1,14 +1,14 @@
 package edu.ksu.canvas.errors;
 
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
+import org.apache.hc.core5.http.*;
+import org.apache.hc.core5.http.impl.*;
 
 /**
  * This allows additional specific behaviour for handling errors.
  */
 public interface ErrorHandler {
 
-    boolean shouldHandle(HttpRequest httpRequest, HttpResponse httpResponse);
+    boolean shouldHandle(HttpRequest httpRequest, ClassicHttpResponse httpResponse) throws ProtocolException;
 
-    void handle(HttpRequest httpRequest, HttpResponse httpResponse);
+    void handle(HttpRequest httpRequest, ClassicHttpResponse httpResponse);
 }
