02cc2e4c9fb57c45f33378e382b40897bc1897be
daebecodin <136534615+daebecodin@users.noreply.github.com>
2025-07-07 01:47:13 -0700

    Updated configuration pattern

--- src/main/java/edu/ksu/canvas/oauth/OauthTokenRefresher.java
+++ src/main/java/edu/ksu/canvas/oauth/OauthTokenRefresher.java
@@ -2,25 +2,32 @@ package edu.ksu.canvas.oauth;
 
 import com.google.gson.Gson;
 import edu.ksu.canvas.impl.GsonResponseParser;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.ContextBuilder;
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.protocol.HttpClientContext;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 public class OauthTokenRefresher implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private static final Logger LOG = LoggerFactory.getLogger(OauthTokenRefresher.class);
 
-    private static final int TIMEOUT_SECONDS = 10;
+    private static final Long TIMEOUT_SECONDS = 10L;
     private final String clientId;
     private final String clientSecret;
     private final String canvasUrl;
@@ -31,32 +38,51 @@ public class OauthTokenRefresher implements Serializable {
         this.canvasUrl = canvasUrl;
     }
 
-    public TokenRefreshResponse getNewToken(String refreshToken) throws IOException {
+    public TokenRefreshResponse getNewToken(String refreshToken) throws IOException, ParseException {
         LOG.debug("Getting a fresh OAuth access token");
-        RequestConfig config = RequestConfig.custom()
-                .setConnectTimeout(TIMEOUT_SECONDS*1000)
-                .setSocketTimeout(TIMEOUT_SECONDS*1000)
+
+        // request configurations
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setResponseTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
                 .build();
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setDefaultRequestConfig(config)
+
+        // custom connection manager
+        final PoolingHttpClientConnectionManager connManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .build();
+        // connection configurations; validation after 10 seconds
+        connManager.setDefaultConnectionConfig(ConnectionConfig.custom()
+                .setConnectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .build()
+
+        );
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(connManager)
+                .build();
+
+        final HttpClientContext context = ContextBuilder.create()
                 .build();
 
         String url = canvasUrl + "/login/oauth2/token?grant_type=refresh_token&client_id=" + clientId + "&client_secret=" + clientSecret + "&refresh_token=" + refreshToken;
         HttpPost postRequest = new HttpPost(url);
 
         try {
-            HttpResponse httpResponse = httpClient.execute(postRequest);
-            int statusCode = httpResponse.getStatusLine().getStatusCode();
+            ClassicHttpResponse httpResponse = httpClient.execute(postRequest, context, response -> {
+                EntityUtils.consume(response.getEntity());
+                return null;
+            });
+            int statusCode = httpResponse.getCode();
             if (statusCode == 401) {
                 LOG.error("Unauthorized refresh token request. Wrong client_id or secret?");
                 return null;
             }
             if (statusCode != 200) {
-                LOG.error("Non-200 status code ( " + statusCode + " )returned while requesting an access token at URL " + url);
+                LOG.error("Non-200 status code ( {} )returned while requesting an access token at URL {}", statusCode, url);
                 HttpEntity errorEntity = httpResponse.getEntity();
                 if (errorEntity != null) {
                     String errorBody = EntityUtils.toString(errorEntity);
-                    LOG.error("Response from Canvas: " + errorBody);
+                    LOG.error("Response from Canvas: {}", errorBody);
                 }
                 return null;
             }
@@ -65,7 +91,6 @@ public class OauthTokenRefresher implements Serializable {
             Gson gson = GsonResponseParser.getDefaultGsonParser(false);
             return gson.fromJson(responseBody, TokenRefreshResponse.class);
         } finally {
-            postRequest.releaseConnection();
             httpClient.close();
         }
     }
